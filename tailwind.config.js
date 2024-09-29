c693bfc2ee16b3c38b106b4b0d320a20ab2727e2
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-09-29 14:05:43 -0300

    feat(Tailwind config): Adição de cor do projeto.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customRed: '#DC0A2D',
+      }
+    },
   },
   plugins: [],
 };
