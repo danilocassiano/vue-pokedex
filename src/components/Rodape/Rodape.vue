b1c165f14e04045b18822370f9c583de1b30f7e5
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-09-29 19:41:33 -0300

    feat(Rodape): Criação do Rodapé.

---
+++ src/components/Rodape/Rodape.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer class="flex justify-end bg-customRed px-6 py-3">
+      <span class="text-white text-sm">Todos os Direitos Reservados &copy; {{ currentYear }}</span>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    computed: {
+      currentYear() {
+        return new Date().getFullYear();
+      }
+    }
+  }
+  </script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,18 +1,21 @@
 <template>
-    <div class="min-h-screen flex flex-col bg-customRed">     
-      <Navbar />    
-    </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent } from 'vue';
-  import Navbar from "../components/Navbar/Navbar.vue" 
-  export default defineComponent({
-    name: 'Main',
-    components: {
-      Navbar,     
-  }
+  <div class="min-h-screen flex flex-col bg-customRed">     
+    <Navbar />
+    <Rodape />    
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Navbar from '../components/Navbar/Navbar.vue';
+import Rodape from '../components/Rodape/Rodape.vue';
+
+export default defineComponent({
+  name: 'Main',
+  components: {
+    Navbar,
+    Rodape,     
+}
 });
 
-  </script>
- 
+</script>
