2e9c9fdde492306a79c8f1548cf9ab5a801c89ed
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-09-29 14:07:37 -0300

    feat(main): Criação do arquivo Main.vue e configuração inicial com o App.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
-    <h1 className="bg-red-500 text-4xl font-bold">Pokedex</h1>
-  </div>
+  <Main /> 
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue';
+import Main from '../src/views/Main.vue';
 
+export default defineComponent({
+  name: 'App',
+  components: {
+    Main,
+  },
+});
 </script>
---
+++ src/views/Main.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="min-h-screen flex flex-col bg-customRed">     
+      <Navbar />    
+    </div>
+</template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'Main',   
+});
+  </script>
+ 
